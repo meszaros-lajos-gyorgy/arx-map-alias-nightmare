f42d3b8c22ea6b36a4985e587067dca8abc25ec6
Lajos Meszaros <m_lajos@hotmail.com>
2023-11-29 16:47:27 +0100

    feat(colors): restore original first version of red from old hacktoberfest

--- src/colors.ts
+++ src/colors.ts
@@ -1,4 +1,4 @@
 import { Color } from 'arx-level-generator'
 
 export const rainyGrey = Color.fromCSS('hsla(226, 10%, 45%, 1)')
-export const twinPeaksRed = Color.fromCSS('#c70430')
+export const twinPeaksRed = Color.fromCSS('#230907')
--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 import { rainyGrey, twinPeaksRed } from './colors.js'
 import { musicBySirs, thunderstorm } from './sounds.js'
 
-// export const MAIN_COLOR = twinPeaksRed
-// export const MAIN_AMBIENCE = musicBySirs
+export const MAIN_COLOR = twinPeaksRed
+export const MAIN_AMBIENCE = musicBySirs
 
-export const MAIN_COLOR = rainyGrey
-export const MAIN_AMBIENCE = thunderstorm
+// export const MAIN_COLOR = rainyGrey
+// export const MAIN_AMBIENCE = thunderstorm
